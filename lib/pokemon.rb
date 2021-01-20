0c8f4ce653e6486b11ce1b7e12d3e078c9f3c24e
David Rafe <david.rafe@gmail.com>
2021-01-20 17:59:17 -0500

    commit tree c954d1e0dc75f4a21bf31085cf9840bcc8347e83

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,30 @@
 class Pokemon
+    attr_accessor :id, :name, :type, :db 
+    def initialize(id:, name:, type:, db:)
+        @id = id
+        @name = name
+        @type = type 
+        @db = db 
+    end
+
+    def self.save(name, type ,db)
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type)
+            VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+        db.execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
+    end
+
+    def self.find(id, db)
+        sql = <<-SQL
+            SELECT *
+            FROM pokemon
+            WHERE id = ?
+            LIMIT 1
+        SQL
+        record = db.execute(sql, id)[0]
+        # binding.pry
+        self.new(id:record[0], name:record[1], type:record[2], db:db)
+    end
 end
